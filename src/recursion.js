113b4b0a5d5eb19c74268f99a1bbea764c70c09b
Nahla Galal <nahlaglal@gmail.com>
2022-03-08 00:13:32 +0200

    Add multiply function (12)

--- src/recursion.js
+++ src/recursion.js
@@ -106,7 +106,15 @@ var modulo = function (x, y) {
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
-var multiply = function (x, y) {};
+var multiply = function (x, y) {
+  if(y < 0) {
+    if(x < 0) return multiply(-x, -y);
+    else return -multiply(x, -y);
+  }
+  if(y === 0) return 0;
+  if(y === 1) return x;
+  return x + multiply(x, y - 1);
+};
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
