e96902ac6d4aedb08b113811863dc7614b5cf2a4
Nahla Galal <nahlaglal@gmail.com>
2022-03-14 23:11:28 +0200

    Add count occurence function (20)

--- src/recursion.js
+++ src/recursion.js
@@ -189,7 +189,11 @@ var fizzBuzz = function (n) {
 // 20. Count the occurrence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
-var countOccurrence = function (array, value) {};
+var countOccurrence = function (array, value) {
+  if(!array.length) return 0;
+  if(array[0] === value) return 1 + countOccurrence(array.slice(1), value);
+  else return countOccurrence(array.slice(1), value)
+};
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
